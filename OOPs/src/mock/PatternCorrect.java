a7dfca6072a8d41ec4002366649b6516998d8302
PRADEEPGUNDLURU <pradeepgundluru077@gmail.com>
2019-09-11 18:05:18 +0530

    Update PatternCorrect.java

--- OOPs/src/mock/PatternCorrect.java
+++ OOPs/src/mock/PatternCorrect.java
@@ -5,9 +5,9 @@ public class PatternCorrect
 
 	public static void main(String[] args) 
 	{
-		for(int i=1;i<=6;i++)
+		for(int i=1;i<=3;i++)
 		{
-			for(int j=1;j<=20;j++)
+			for(int j=1;j<=7;j++)
 			{
 				if(j%2==0)
 				{ 
